639063d4b3618f7256752efd03f6d88104b7897c
SriSathyaSaiVidyaVahini <hari.n@srisathyasaividyavahini.org>
2021-03-03 21:32:04 +0530

    fixes back button and recent courses

--- app/src/main/java/com/srisathyasaividyavahini/lmslite/MainActivity.kt
+++ app/src/main/java/com/srisathyasaividyavahini/lmslite/MainActivity.kt
@@ -10,15 +10,19 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myWebView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView = findViewById(R.id.webview)
         myWebView.webViewClient = WebViewClient()
         myWebView.getSettings().setJavaScriptEnabled(true)
-        myWebView.loadUrl("https://sssvidyavahini.org")
+        myWebView.getSettings().setDomStorageEnabled(true)
+		myWebView.loadUrl("https://sssvidyavahini.org")
     }
-/*
+
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         // Check if the key event was the Back button and if there's history
         if (keyCode == KeyEvent.KEYCODE_BACK && myWebView.canGoBack()) {
@@ -28,5 +32,5 @@ class MainActivity : AppCompatActivity() {
         // If it wasn't the Back key or there's no web page history, bubble up to the default
         // system behavior (probably exit the activity)
         return super.onKeyDown(keyCode, event)
-    } */
+    } 
 }
