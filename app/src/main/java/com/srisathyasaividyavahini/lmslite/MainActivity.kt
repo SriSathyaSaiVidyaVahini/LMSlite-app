cee86bb85d0ded9785aa20817b016055d28f9def
SriSathyaSaiVidyaVahini <hari.n@srisathyasaividyavahini.org>
2021-03-05 11:22:01 +0530

    fixes downloading

--- app/src/main/java/com/srisathyasaividyavahini/lmslite/MainActivity.kt
+++ app/src/main/java/com/srisathyasaividyavahini/lmslite/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.srisathyasaividyavahini.lmslite
 
+import android.app.DownloadManager
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.view.KeyEvent
-import android.webkit.WebSettings
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +23,21 @@ class MainActivity : AppCompatActivity() {
         myWebView.getSettings().setJavaScriptEnabled(true)
         myWebView.getSettings().setDomStorageEnabled(true)
 		myWebView.loadUrl("https://sssvidyavahini.org")
+		myWebView.setDownloadListener({ url, userAgent, contentDisposition, mimeType, contentLength ->  
+			val request = DownloadManager.Request(Uri.parse(url))
+			request.setMimeType(mimeType)  
+			request.addRequestHeader("cookie", CookieManager.getInstance().getCookie(url))
+			request.addRequestHeader("User-Agent", userAgent)  
+			request.setDescription("Downloading file...")  
+			request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType))
+			//request.allowScanningByMediaScanner()  // deprecated
+			request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)  
+			request.setDestinationInExternalFilesDir(this@MainActivity, Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(
+                                url, contentDisposition, mimeType))  
+			val dm = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+			dm.enqueue(request)  
+			Toast.makeText(applicationContext, "Downloading File", Toast.LENGTH_LONG).show()
+		})
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
--- readme.txt
+++ readme.txt
@@ -2,9 +2,13 @@ A lite LMS app using Kotlin and WebView.
 
 Created in Android Studio 4.1.2
 
+Hitting Alt+Enter for modules not yet imported and highlighted in red automatically imports.
+
 References:
 https://developer.chrome.com/docs/multidevice/webview/gettingstarted/
 https://developer.android.com/codelabs/basic-android-kotlin-training-change-app-icon#0
 https://developer.android.com/guide/webapps/webview.html#kotlin
 https://stackoverflow.com/questions/60641759/how-to-open-webview-links-within-app-android-kotlin
 https://stackoverflow.com/questions/40756794/ajax-not-working-in-android-webview
+https://www.c-sharpcorner.com/article/how-to-add-download-listener-to-webview-in-kotlin/
+https://devofandroid.blogspot.com/2018/02/downloading-file-from-android-webview.html
